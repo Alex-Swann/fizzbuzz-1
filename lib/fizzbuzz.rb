7da3f90304d879935b79aba5779f0bce43602f97
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-17 10:56:25 +0100

    all files for app

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,21 +1,39 @@
-def fizzbuzz(number)
-  return "Fail! Not an integer" if /[^-0-9]/.match(number.to_s)
-  return "Integer outside of range. Try again!" if !(1..100).include? number
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
 
-  if number % 3 == 0
-    if number % 5 == 0
-			puts 'fizzbuzz'
-      return 'fizzbuzz'
-		else
-			puts 'fizz'
-      return 'fizz'
-		end
-	elsif number % 5 == 0
-		puts 'buzz'
-    return 'buzz'
-	else
-		puts number.to_s
-    return number.to_s
-	end
+class FizzBuzzApp < Sinatra::Base
+  get '/' do
+    def fizzbuzz(number)
+      exit if number.downcase == "end"
+      return puts "Fail! Not an integer" if /[^-0-9]/.match(number.to_s)
+      number = number.to_i
+      return puts "Integer outside of range. Try again!" if !(1..100).include? number
+
+      if number % 3 == 0
+        if number % 5 == 0
+    			puts 'fizzbuzz'
+          return 'fizzbuzz'
+    		else
+    			puts 'fizz'
+          return 'fizz'
+    		end
+    	elsif number % 5 == 0
+    		puts 'buzz'
+        return 'buzz'
+    	else
+    		puts number.to_s
+        return number.to_s
+    	end
+    end
+    while true
+      puts "Please input number or 'end' to quit!"
+      input = gets.chomp
+      fizzbuzz(input)
+      puts
+    end
+  end
 end
+
+
 
