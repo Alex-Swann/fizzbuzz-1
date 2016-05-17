709f02f25f65cb4105e46ae920a9659a632b4010
Alex-Swann <alex_swann@hotmail.co.uk>
2016-05-17 15:44:40 +0100

    corrected puts

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,24 +2,20 @@
 module FizzBuzzApp
     def self.fizzbuzz(number)
       puts "Please input number!"
-      return puts "Fail! Not an integer" if /[^-0-9]/.match(number.to_s)
+      return "Fail! Not an integer" if /[^-0-9]/.match(number.to_s)
       number = number.to_i
-      return puts "Integer outside of range. Try again!" if !(1..100).include? number
+      return "Integer outside of range. Try again!" if !(1..100).include? number
 
       if number % 3 == 0
         if number % 5 == 0
-    			puts 'fizzbuzz'
-          return 'fizzbuzz'
+          'fizzbuzz'
     		else
-    			puts 'fizz'
-          return 'fizz'
+          'fizz'
     		end
     	elsif number % 5 == 0
-    		puts 'buzz'
-        return 'buzz'
+        'buzz'
     	else
-    		puts number.to_s
-        return number.to_s
+        number.to_s
     	end
     end
 end
