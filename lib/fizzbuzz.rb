15758aef0a056efddf1036e35cc07a6f6edcf5c4
danielschambers <daniel.s.chambers@gmail.com>
2016-05-17 17:16:13 +0100

    Edited boundaries and amended ruby code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,28 +1,20 @@
 
 module FizzBuzzApp
-    def self.fizzbuzz(number)
-      puts "Please input number!"
-      return puts "Fail! Not an integer" if /[^-0-9]/.match(number.to_s)
-      number = number.to_i
-      return puts "Integer outside of range. Try again!" if !(1..100).include? number
+  def self.fizzbuzz(number)
+    return "Fail! Not an integer" if /[^-0-9]/.match(number.to_s)
+    number = number.to_i
+    return"Integer outside of range. Try again!" if !(1..100).include? number
 
-      if number % 3 == 0
-        if number % 5 == 0
-    			puts 'fizzbuzz'
-          return 'fizzbuzz'
-    		else
-    			puts 'fizz'
-          return 'fizz'
-    		end
-    	elsif number % 5 == 0
-    		puts 'buzz'
-        return 'buzz'
-    	else
-    		puts number.to_s
-        return number.to_s
-    	end
+    if number % 3 == 0
+      if number % 5 == 0
+        'fizzbuzz'
+      else
+        'fizz'
+      end
+    elsif number % 5 == 0
+      'buzz'
+    else
+      number.to_s
     end
+  end
 end
-
-
-
